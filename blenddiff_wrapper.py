c8f8c69df22d410b06faad5ca14168f0bbaf5f3b
Chris Margach <hcagramc@gmail.com>
2025-07-09 16:26:39 +0900

    Reuse arg logic

--- blenddiff_wrapper.py
+++ blenddiff_wrapper.py
@@ -3,10 +3,10 @@ import argparse, logging
 import subprocess
 import json
 
+from blenddiff import BlendDiffParser
 
-logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 LOG = logging.getLogger(__name__)
-
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
 def run_diff(blender_exec: str, args: list[str]):
     cmd = [
@@ -37,14 +37,27 @@ if __name__ == "__main__":
     _BLENDER_EXEC_LABEL = "--blender-exec"
 
     ap = argparse.ArgumentParser(description="Wrapper for running blenddiff via Blender.")
-    # ap.add_argument("--file-original", required=True, help="Path to the original .blend file.")
-    # ap.add_argument("--file-modified", required=True, help="Path to the modified .blend file.")
-    # ap.add_argument("--id-prop", help="Optional ID property to use for datablock identity.")
     ap.add_argument(_BLENDER_EXEC_LABEL, help="Path to the Blender executable.", required=True)
-    # ap.add_argument("--pretty-json", action="store_true", help="Output JSON in pretty format.")
-    # ap.add_argument("--debug", action="store_true", help="Enable debug logging to stdout.")
+    ap.add_argument(
+        "--wrapper-log-level",
+        help="The log level for the wrapper.",
+        required=False,
+        choices=[name for name in logging._nameToLevel.keys() if name in {"CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"}]
+    )
 
     args, argv = ap.parse_known_args() 
+    LOG.debug(f"Got args: {args}")
+    LOG.debug(f"Got argv: {argv}")
+
+    if args.wrapper_log_level:
+        LOG.setLevel(args.wrapper_log_level)
+
+    bd_args = BlendDiffParser()
+    try:
+        bd_args.parse_args(argv)
+    except Exception as e:
+        LOG.error(f"Error parsing blenddiff arguments: {e}")
+        sys.exit(1)
 
     result = run_diff(
         blender_exec = args.blender_exec,
